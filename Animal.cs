58b6f7bbec626d536e62e52e7a642cd841f527b7
Talha Imam <smtalhaimam@gmail.com>
2017-02-05 17:33:19 +0500

    OOP Real life example depicted through classes, interfaces

---
+++ Animal.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task1
+{
+    interface ILivingThings
+    {
+        void Breathing();
+        void CanMove();
+    }
+    interface IAnimals : ILivingThings
+    {
+        void HaveLegs();
+        void HaveEyes();
+        void HaveMouth();
+    }
+    abstract class Mammal : IAnimals
+    {
+        public void Breathing()
+        {
+            Console.WriteLine("They can breathe");
+        }
+        public void CanMove()
+        {
+            Console.WriteLine("They can move");
+        }
+        public void HaveLegs()
+        {
+            Console.WriteLine("They have legs");
+        }
+        public void HaveEyes()
+        {
+            Console.WriteLine("They have eyes");
+        }
+        public void HaveMouth()
+        {
+            Console.WriteLine("They have mouth");
+        }
+        public abstract void Fly();
+    }
+    class Bats : Mammal
+    {
+        public override void Fly()
+        {
+            Console.WriteLine("Bats can fly");
+        }
+    }
+    class FourLeggedMammal : Mammal
+    {
+        public override void Fly()
+        {
+            Console.WriteLine("Four Legged Mammals cannot fly");
+        }
+        public virtual void Type()
+        {
+            Console.WriteLine("Some are carnivores and some are hebrivores");
+        }
+    }
+    class Lion : FourLeggedMammal
+    {
+        public override void Type()
+        {
+            base.Type();
+            Console.WriteLine("Lion is a carnivores");
+        }
+    }
+    class Goat : FourLeggedMammal
+    {
+        public override void Type()
+        {
+            base.Type();
+            Console.WriteLine("Goat is a Hebrivores");
+        }
+    }
+}
---
+++ BigIntegers.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+
+namespace Task1
+{
+    class Calculate
+    {
+        static int count=0;
+        BigInteger number1, number2;
+        public Calculate(string n1, string n2)
+        {
+            this.number1 = BigInteger.Parse(n1);
+            this.number2 = BigInteger.Parse(n2);
+            count++;
+        }
+        public void Add()
+        {
+            BigInteger number3 = number1 + number2;
+            Console.WriteLine("Addition of Big Integers:\nFunction: " + BigInteger.Add(number1, number2));
+            Console.WriteLine("Simple:   " + number3+"\n");
+        }
+        public void Add(string n3) //An addition method that takes a new number as an input and subtracts from the existing number1
+        {
+            BigInteger number3 = BigInteger.Parse(n3);
+            BigInteger number4 = number1 - number3;
+            Console.WriteLine("Subtraction of Big Integers:\nFunction: "+BigInteger.Subtract(number1,number3));
+            Console.WriteLine("Simple:   " + number4);
+        }
+        public void ObjectCount()
+        {
+            Console.WriteLine("\nThis is the object count: "+count+"\n");
+        }
+    }
+    class BigIntegers
+    {
+        static void Main(string[] args)
+        {
+            string FirstNum = "12349872892389423897234789234987234892348972348972389742";
+            string ThirdNum = "88900878766309097877089089989";
+            string FourthNum = "1234987289238942389723478923498723489234897";
+            
+            Calculate c1 = new Calculate(FirstNum, FirstNum); //Same Sized Numbers
+            c1.Add();
+            Calculate c2 = new Calculate(FirstNum, ThirdNum); //Different sized numbers
+            c2.Add();
+            c2.Add(FourthNum); //Subtraction
+            c2.ObjectCount();
+
+            int c = 8 % 10;
+            Console.WriteLine(c);
+
+            //From Animal Class
+            Goat m1 = new Goat();
+            m1.Breathing();
+            m1.CanMove();
+            m1.Fly();
+            m1.Type();
+        }
+    }
+}
